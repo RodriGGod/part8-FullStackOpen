e850fcdda2fca791df08bb3600a3d06f4ee43dfb
xGGOD_ <rblo2002@gmail.com>
2025-09-16 12:03:40 +0200

    8c finished

--- frontend/src/components/PhoneForm.jsx
+++ frontend/src/components/PhoneForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useMutation } from '@apollo/client'
 
 import { EDIT_NUMBER } from '../queries'
@@ -6,9 +6,9 @@ import { EDIT_NUMBER } from '../queries'
 const PhoneForm = () => {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
+  const [changeNumber, result] = useMutation(EDIT_NUMBER)
 
-
-  const [ changeNumber ] = useMutation(EDIT_NUMBER)
+  
 
   const submit = (event) => {
     event.preventDefault()
